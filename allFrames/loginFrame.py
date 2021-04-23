10b1adff43e03785571565babca9ee82ca8bcbf9
Manish <manishjalui11@gmail.com>
2021-04-23 13:33:42 +0530

    Changed few lines
    changes in loginFrame.py pwmanger.py files

--- allFrames/loginFrame.py
+++ allFrames/loginFrame.py
@@ -1,5 +1,6 @@
+from allFrames.forgotPassFrame import forgotPass
 import tkinter as tk
-from allFrames import forgotPassFrame
+
 
 class loginFrame():
 	def __init__(self, rootName):
@@ -8,16 +9,16 @@ class loginFrame():
 		self.entryFont = ("Rockwell", 16)
 		self.root = rootName
 
-		self.masterFrame = tk.LabelFrame(self.root, text="Home", bd=5)
-		self.masterFrame.place(relx=0, rely=0, relwidth=1, relheight=1)
+		self.loginFrame = tk.LabelFrame(self.root, text="Home", bd=5)
+		self.loginFrame.place(relx=0, rely=0, relwidth=1, relheight=1)
 
-		self.mpassentry = tk.Entry(self.masterFrame, show = "*", width = 20, font = self.entryFont)
+		self.mpassentry = tk.Entry(self.loginFrame, show = "*", width = 20, font = self.entryFont)
 		self.mpassentry.place(relx=0.275, rely=0.384, relwidth=0.45, relheight=0.07)
 
-		self.mpassenter = tk.Button(self.masterFrame, text = "Enter", command = self.checkPass, font = self.labelFont)
+		self.mpassenter = tk.Button(self.loginFrame, text = "Enter", command = self.checkPass, font = self.labelFont)
 		self.mpassenter.place(relx=0.35, rely=0.52, relwidth=0.3, relheight=0.1)
 
-		self.forgotPass = tk.Button(self.masterFrame, text = "Forgot Password", command = self.forgot)
+		self.forgotPass = tk.Button(self.loginFrame, text = "Forgot Password", command = self.forgot)
 		self.forgotPass.place(relx=0.35, rely=0.7, relwidth=0.35, relheight=0.08)
 
 		self.mpassentry.bind("<Return>", self.shortcuts)
@@ -35,11 +36,11 @@ class loginFrame():
 	def checkPass(self):
 		mp = self.mpassentry.get()
 		if mp == "admin":
-			confirmLabel = tk.Label(self.masterFrame, text = "Login Successful", bg = 'Grey', font = self.labelFont)
+			confirmLabel = tk.Label(self.loginFrame, text = "Login Successful", bg = 'Grey', font = self.labelFont)
 			confirmLabel.place(relx=0.16, rely=0.02, relwidth=0.7, relheight=0.05)
 			confirmLabel.after(2000, confirmLabel.destroy)
 			return
-		errorLabel = tk.Label(self.masterFrame, text = "Wrong Password... try again", bg = 'Grey', font = self.labelFont)
+		errorLabel = tk.Label(self.loginFrame, text = "Wrong Password... try again", bg = 'Grey', font = self.labelFont)
 		errorLabel.place(relx=0.16, rely=0.02, relwidth=0.7, relheight=0.05)
 		errorLabel.after(2000, errorLabel.destroy)
 
--- pwmanager.pyw
+++ pwmanager.pyw
@@ -1,12 +1,12 @@
 import tkinter as tk
 from allFrames.loginFrame import loginFrame
-# from allFrames.forgotPassFrame import forgotPass
+from allFrames.forgotPassFrame import forgotPass
 
 root = tk.Tk()
 root.geometry("350x400+600+150")
 root.title("Password Manager")
 
+forgotPassObj = forgotPass(root)
 loginObj = loginFrame(root)
-# forgotPassObj = forgotPass(root)
 
 root.mainloop()
